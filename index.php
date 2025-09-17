021353181d2be8de9582c2c935f07038cb7e15be
Sushrutrai <sushrutrai99@gmail.com>
2025-09-17 18:46:30 +0545

    Qr ko pachi soch aile lai landing page bana

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>BookIt</title>
+</head>
+<body>
+    <header>
+    <h1 id="logo">BOOKIT</h1>
+    <nav>
+        <ul id="navigation">
+            <li><a href="">About us</a></li>
+            <li><a href="">Explore</a></li>
+            <li><a href="">My Events</a></li>
+            <li><a href="">Sign up</a></li>
+        </ul>
+    </nav>
+    </header>
+    
+</body>
+</html>
